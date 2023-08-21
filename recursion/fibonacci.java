27981df3b0460c8222a2bc47248290bad2565ab0
Divya Madhyan <divyamadhyan@Divyas-MacBook-Air.local>
2023-08-21 17:51:48 +0530

    renamed with number and added few files

---
+++ recursion/fibonacci.java
@@ -0,0 +1,17 @@
+//https://www.algoexpert.io/questions/nth-fibonacci
+
+import java.util.*;
+
+class Program {
+  public static int getNthFib(int n) {
+    // Write your code here.
+    if(n == 1) 
+      return 0;
+    if(n ==2) 
+      return 1;
+
+    int ans = getNthFib(n -1) + getNthFib(n - 2);
+    return ans;
+    
+  }
+}
